e6d1c17a7ae38f484b0769bfdd9d038215d80122
Karl Pokus <karl.pokus@gmail.com>
2019-11-27 14:04:09 +0100

    make client exit 1 on err

--- client.js
+++ client.js
@@ -10,17 +10,19 @@ log.out(`calling ${ url }`);
 http.get(url, res => {
 	let data = '';
 	res
-		.on("error", err => {
-			log.out("Error: " + err.message);
+		.on('error', err => {
+			log.out(`err: ${ err.message }`);
+			process.exit(1);
 		})
-		.on('data', (chunk) => {
+		.on('data', chunk => {
 			data += chunk;
 		})
 		.on('end', () => {
-			log.out(data);
+			log.out(`response: ${ data }`);
 		});
 }).on('error', err => {
-	log.out("Error: " + err.message);
+	log.out(`err: ${ err.message }`);
+	process.exit(1);
 });
 
 // short delay before sending ready to allow for the request to arrive
